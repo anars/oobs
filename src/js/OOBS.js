dadb7bf17aab1048c4d9bad34cee73dbf01f4111
Kay Anar <git@anars.com>
2019-11-13 00:18:35 -0500

    initial files

---
+++ src/js/OOBS.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// eslint-disable-next-line no-unused-vars
+class OOBS {
+
+    constructor (options = {}) {}
+
+}
---
+++ src/js/containers/OOBSContainer.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// eslint-disable-next-line no-unused-vars
+class OOBSContainer extends OOBS {
+
+    constructor (options = {}) {}
+
+}
