4c38b2a4cb8893fa6d5f42ea8a3da23981bd5f54
Kay Anar <git@anars.com>
2019-11-13 13:52:55 -0500

    empty class attribute and dimension wapper fuctions

--- src/js/OOBS.js
+++ src/js/OOBS.js
@@ -1,8 +1,62 @@
 "use strict";
 
+/* eslint-disable padded-blocks */
+
 // eslint-disable-next-line no-unused-vars
 class OOBS {
-
     constructor (options = {}) {}
+
+    /**
+     *
+     * Document it
+     *
+     * @param  {String} name Document it
+     * @param  {String} value Document it
+     *
+     * @returns {OOBS} Document it
+     */
+    css (name, value) {
+        return this;
+    }
+
+    addClass (value) {
+        return this;
+    }
+
+    removeClass (value) {
+        return this;
+    }
+
+    toggleClass (value, state) {
+        return this;
+    }
+
+    hasClass (className) {
+        return this;
+    }
+
+    height (value) {
+        return this;
+    }
+
+    innerHeight (value) {
+        return this;
+    }
+
+    innerWidth (value) {
+        return this;
+    }
+
+    outerHeight (value, includeMargin) {
+        return this;
+    }
+
+    outerWidth (value, includeMargin) {
+        return this;
+    }
+
+    width (value) {
+        return this;
+    }
 
 }
